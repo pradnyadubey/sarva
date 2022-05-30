7f823da5e076be86c88b08a4042b9d9a0dee7642
pradnyadubey <94223604+pradnyadubey@users.noreply.github.com>
2022-05-30 17:24:48 +0530

    Add files via upload

---
+++ src/test/java/com/synoptek/app/sarva/SarvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synoptek.app.sarva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
