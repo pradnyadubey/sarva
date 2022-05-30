d519dcae0f3549a03f5606a34a8809ac33a93bd3
pradnyadubey <94223604+pradnyadubey@users.noreply.github.com>
2022-05-30 14:51:30 +0530

    Add files via upload

---
+++ sarva/src/test/java/com/cke/app/sarva/SarvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cke.app.sarva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
